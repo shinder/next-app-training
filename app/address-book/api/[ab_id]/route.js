76517471a560c666bacd85c1913b782ef77a7ec7
Shinder Lin <shinder.lin@gmail.com>
2025-01-05 11:36:33 +0800

    address-book 更新

--- app/address-book/api/[ab_id]/route.js
+++ app/address-book/api/[ab_id]/route.js
@@ -1,6 +1,9 @@
 import db from "@/utils/connect-mysql";
 import { responseJson } from "@/utils/my-parsers";
+import { fmDate } from "@/utils/my-schemas";
 import moment from "moment";
+import {getJsonBody} from "@/utils/my-parsers";
+import { mySchema } from "@/utils/my-schemas";
 
 // *** 取得列表資料
 export const GET = async (request, { params }) => {
@@ -19,15 +22,44 @@ export const GET = async (request, { params }) => {
   let birthday = moment(rows[0].birthday);
   birthday = birthday.isValid() ? birthday.format(fmDate) : "";
   output.success = true;
-  output.data = { ...rows[0], birthday};
+  output.data = { ...rows[0], birthday };
   return responseJson(output);
 };
 
 // *** 取得列表資料
 export const PUT = async (request, { params }) => {
-  // console.log("---", request.constructor.name, "---"); // NextRequest
-  // console.log(request);
-  const obj = await getListData(request);
-  const success = !obj.redirect;
-  return responseJson({});
+  const body = await getJsonBody(request);
+  const output = {
+    success: false,
+    bodyData: body,
+    errors: [],
+  };
+  const { name, email, mobile, birthday, address } = body;
+  const data = { name, email, mobile, birthday, address };
+
+  const checkResult = mySchema.safeParse(data);
+  if (!checkResult.success) {
+    // 沒有通過資料檢查
+    output.errors = checkResult.error.issues;
+    return responseJson(output);
+  }
+
+  // 處理生日沒有填寫的情況
+  const m = moment(data.birthday);
+  if (!m.isValid()) {
+    data.birthday = null;
+  }
+
+  const sql = "UPDATE `address_book` SET ? WHERE ab_id=? ";
+  try {
+    const [result] = await db.query(sql, [data, params.ab_id]);
+    output.result = result;
+    output.success = !!result.changedRows;
+    // result.affectedRows: 條件找到的筆數
+    // result.changedRows: 真正資料有變更的筆數
+  } catch (ex) {
+    output.ex = ex;
+  }
+
+  return responseJson(output);
 };
